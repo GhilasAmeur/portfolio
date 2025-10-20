86c50d2ed7070c9b8097fd8195c9fba1396c4aea
GhilasAmeur <ghilasameur@yahoo.com>
2025-10-20 18:21:54 +0200

    Ajout de l'image dans le composant APropos

--- src/componenets/Accueil.jsx
+++ src/componenets/Accueil.jsx
@@ -1,7 +1,5 @@
-
-import { TypeAnimation } from 'react-type-animation';
-import imgTD from '../assets/td.jpg';
-
+import { TypeAnimation } from "react-type-animation";
+import imgTD from "../assets/td.jpg";
 
 const Accueil = () => {
   return (
@@ -11,42 +9,46 @@ const Accueil = () => {
     >
       {/* --- Partie gauche : texte --- */}
       <div className="md:w-1/2 space-y-6">
-
-       <h2 className="text-4xl font-bold mb-4">
-  <TypeAnimation
-    sequence={[
-      'Bonjour 😊', 2000,
-      'Bienvenue sur mon site ⌨', 2000,
-      'Explorez et amusez-vous !', 2000,
-    ]}
-    wrapper="span"
-    speed={50}
-    repeat={Infinity}
-  />
-</h2>
-        <h1>
-       </h1>
+        <h2 className="text-4xl font-bold mb-4">
+          <TypeAnimation
+            sequence={[
+              "Bonjour 😊",
+              2000,
+              "Bienvenue sur mon site ⌨",
+              2000,
+              "Explorez et amusez-vous !",
+              2000,
+            ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+          />
+        </h2>
+        <h1></h1>
         <h3 className="text-2xl font-semibold">
           Je suis <span className="text-purple-400">Développeur web</span>
         </h3>
-        
+
         <p className="text-gray-300 leading-relaxed">
           Développeur web passionné, j’aime créer des interfaces modernes et
-          intuitives. J’utilise principalement <span className="text-purple-400">React</span> et{" "}
+          intuitives. J’utilise principalement{" "}
+          <span className="text-purple-400">React</span> et{" "}
           <span className="text-purple-400">Tailwind CSS</span> pour concevoir
           des applications performantes et élégantes. Mon objectif est de
           proposer des solutions efficaces et adaptées à chaque besoin.
         </p>
-       <a  href="mecontacter"><button className="mt-4 bg-purple-600 hover:bg-purple-700 text-white px-6 py-2 rounded-full transition duration-300">
-          Me contacter
-        </button></a> 
+        <a href="mecontacter">
+          <button className="mt-4 bg-purple-600 hover:bg-purple-700 text-white px-6 py-2 rounded-full transition duration-300">
+            Me contacter
+          </button>
+        </a>
       </div>
 
       {/* --- Partie droite : image --- */}
       <div className="md:w-1/2 flex justify-center mt-10 md:mt-0">
         <div className="relative w-64 h-64 md:w-80 md:h-80">
           <img
-           src={imgTD}
+            src={imgTD}
             alt=""
             className="rounded-full object-cover w-full h-full border-4 border-purple-500 shadow-lg hover:scale-105 transition-transform duration-500"
           />
@@ -56,7 +58,4 @@ const Accueil = () => {
   );
 };
 
-
-
-
-export default Accueil
+export default Accueil;
--- src/pages/APropos.jsx
+++ src/pages/APropos.jsx
@@ -1,27 +1,38 @@
-import { motion } from "framer-motion"
-import imgTD from '../assets/td.jpg'
+import { motion } from "framer-motion";
+import imgTD from "../assets/td.jpg";
 
 export const APropos = () => {
-  return (<motion.div
-
-  initial={{ opacity: 0, y: 50 }}
-  whileInView={{ opacity: 1, y: 0 }}   // <- visible animation
-  transition={{ duration: 0.6, ease: 'easeInOut' }}
-  viewport={{ once: true }}
-  id='about'
-  className="py-20 bg-dark-200"
->
-  <div className="container mx-0 px-6">
-    <h2 className="text-3xl font-bold text-center mb-4">
-      À propos de <span className="text-purple-500">moi</span>
-    </h2>
-    <p className="text-gray-400 text-center max-2xl mx-auto mb-16">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae reprehenderit nesciunt, nihil aut libero culpa ducimus expedita alias, ea omnis fuga quos consequatur reiciendis quasi, labore optio architecto adipisci. Quae.</p>
-  </div>
-  <div className="flex flex-col md:flex-row items-center gap-12">
-    <div className="md:w-1/2 rounded-2x1 ouverflow-hodden  ">
-    <img src={imgTD}alt=""/></div>
-  </div>
-</motion.div>
-)
-   
-}
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }} // <- visible animation
+      transition={{ duration: 0.6, ease: "easeInOut" }}
+      viewport={{ once: true }}
+      id="about"
+      className="py-20 bg-dark-200"
+    >
+      <div className="container mx-0 px-6">
+        <h2 className="text-3xl font-bold text-center mb-4">
+          À propos de <span className="text-purple-500">moi</span>
+        </h2>
+        <p className="text-gray-400 text-center max-2xl mx-auto mb-16">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae
+          reprehenderit nesciunt, nihil aut libero culpa ducimus expedita alias,
+          ea omnis fuga quos consequatur reiciendis quasi, labore optio
+          architecto adipisci. Quae.
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row items-center gap-12">
+        <div className="md:w-1/2 rounded-2xl overflow-hidden">
+          <motion.img 
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }} // <- visible animation
+          transition={{ duration: 0.9, ease: "easeInOut" }}
+          viewport={{ once: false, amount:0.2 }}
+          className="w-full h-full object-cover"
+          src={imgTD} alt="" />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
